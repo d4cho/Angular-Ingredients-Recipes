a13666ec7ae9aaae0e5276cfd899764742d8e956
bioDanCho <daniel.cho@thebioagency.com>
2022-06-21 00:45:45 -0400

    data binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,18 +9,23 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [
         new Recipe(
-            'Test Recipe',
-            'This is a test',
+            'Test Recipe 1',
+            'This is a test 1',
             'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545'
         ),
         new Recipe(
-            'Test Recipe',
-            'This is a test',
+            'Test Recipe 2',
+            'This is a test 2',
             'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545'
         ),
     ];
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
     constructor() {}
 
     ngOnInit(): void {}
+
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
+    }
 }
